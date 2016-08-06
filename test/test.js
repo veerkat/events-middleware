7e8d8f10b73d98086b531c9e3502edaa943d2608
veerkat <wei.zh210@gmail.com>
2016-08-06 15:02:33 +0800

    add test

---
+++ test/test.js
@@ -0,0 +1,47 @@
+/*global define: false,
+ describe: false,
+ assert: false,
+ it: false,
+ beforeEach: false,
+ before: false,
+ after: false*/
+
+'use strict';
+
+const {EventMiddleware, EventEmitter} = require('../index');
+
+describe('events-middleware', function() {
+    describe('EventMiddleware', function() {
+        describe('pre', function() {
+        });
+
+        describe('post', function() {
+        });
+
+        describe('call', function() {
+        });
+
+        describe('catch', function() {
+        });
+    });
+
+    describe('EventEmitter', function() {
+        describe('on', function() {
+        });
+
+        describe('pre', function() {
+        });
+
+        describe('post', function() {
+        });
+
+        describe('preEach', function() {
+        });
+
+        describe('postEach', function() {
+        });
+
+        describe('onError', function() {
+        });
+    });
+});
