a1720deebe6aa882072d55fe906ba657640c1bb2
veerkat <wei.zh210@gmail.com>
2016-08-07 22:52:08 +0800

    update

--- test/test.js
+++ test/test.js
@@ -54,50 +54,43 @@ describe('events-middleware', function() {
         });
 
         describe('call', function() {
-            it('should work with only main function', function() {
+            it('should work with only main function and default options', function() {
                 const m1 = new EventMiddleware('test', function(value, next) {
                     next(null, value + 1);
                 });
-                return m1.call(1)
+                const m2 = new EventMiddleware('test', function(value) {
+                    return Promise.resolve(value + 1);
+                });
+                return Promise.all([
+                    m1.call(1)
                     .then(value => {
                         value.should.be.eql(2);
-                        const m2 = new EventMiddleware('test', function(value) {
-                            return Promise.resolve(value + 1);
-                        });
-                        return m2.call(1, 2);
+                    }),
+                    m2.call(1)
+                    .then(value => {
+                        value.should.be.eql(2);
                     })
-                    .then(value => {
-                        value.should.be.eql(2);
-                    });
+                ]);
             });
 
-            it('should work with pre and main functions', function() {
+            it('should work with pre, main and post functions and default options by next callback', function() {
                 const m = new EventMiddleware('test', function(value, next) {
                     next(null, value + 1);
                 });
-                m.pre(function(value) {
-                    return Promise.resolve(value + 1);
+                m.pre(function(value, next) {
+                    next(null, value + 1);
+                });
+                m.post(function(value, next) {
+                    next(null, value + 1);
                 });
                 return m.call(1).then(value => {
-                    value.should.be.eql(3);
+                    value.should.be.eql(4);
                 });
             });
 
-            it('should work with main and post funcions', function() {
-                const m = new EventMiddleware('test', function(value, next) {
-                    next(null, value + 1);
-                });
-                m.post(function(value) {
+            it('should work with pre, main and post functions and default options by promise', function() {
+                const m = new EventMiddleware('test', function(value) {
                     return Promise.resolve(value + 1);
-                });
-                return m.call(1).then(value => {
-                    value.should.be.eql(3);
-                });
-            });
-
-            it('should work with pre, main and post functions', function() {
-                const m = new EventMiddleware('test', function(value, next) {
-                    next(null, value + 1);
                 });
                 m.pre(function(value) {
                     return Promise.resolve(value + 1);
@@ -110,127 +103,300 @@ describe('events-middleware', function() {
                 });
             });
 
-            it('should pass multiple value to next function ' +
-               'when calling next callback with multiple value', function() {
+            it('should pass multiple value with default options when using next callback', function() {
                 const m = new EventMiddleware('test', function(value1, value2, next) {
                     next(null, value1 + 1, value2 + 1);
                 });
                 m.post(function(value1, value2, next) {
-                    return next(null, value1 + 1, value2 + 1);
+                    next(null, value1 + 1, value2 + 1);
                 });
                 return m.call(1, 2).then(value => {
                     value.should.be.eql([3, 4]);
                 });
             });
 
-            it('should catch error when pre function throw error', function() {
-                const m = new EventMiddleware('test', noop);
-                m.pre(function() {
+            it('should work with globalArgs is true', function() {
+                const m = new EventMiddleware('test', function(g, next) {
+                    next(null, g.value + 1);
+                }, {
+                    globalArgs: true,
+                });
+                m.pre(function(g) {
+                    g.value = g.value + 1;
+                    return Promise.resolve();
+                });
+                m.post(function(g) {
+                    g.value = g.value + 1;
+                    return Promise.resolve();
+                });
+                const g = {
+                    value: 1
+                };
+                return m.call(g).then(_g => {
+                    _g.should.be.deepEqual(g);
+                });
+            });
+
+            it('should work with multiArgs is false', function() {
+                const m = new EventMiddleware('test', function(value, next) {
+                    next(null, value + 1);
+                }, {
+                    multiArgs: false
+                });
+                m.pre(function(value, next) {
+                    return next(null, value + 1, 2, 3);
+                });
+                m.post(function(value, next) {
+                    return next(null, value + 1, 2, 3);
+                });
+                return m.call(1, 2, 3).then(value => {
+                    value.should.be.eql(4);
+                });
+            });
+
+            it('should catch error from pre function', function() {
+                const m1 = new EventMiddleware('test', noop);
+                m1.pre(function() {
                     throw new Error('error');
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
-            });
 
-            it('should catch error when pre function return error by next callback', function() {
-                const m = new EventMiddleware('test', noop);
-                m.pre(function(next) {
+                const m2 = new EventMiddleware('test', noop);
+                m2.pre(function(next) {
                     next(new Error('error'));
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
-            });
 
-            it('should catch error when pre function return error by promise', function() {
-                const m = new EventMiddleware('test', noop);
-                m.pre(function() {
+                const m3 = new EventMiddleware('test', noop);
+                m3.pre(function() {
                     return Promise.reject(new Error('error'));
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
+
+                return Promise.all([
+                    m1.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    }),
+                    m2.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    }),
+                    m3.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    })
+                ]);
             });
 
-            it('should catch error when post function throw error', function() {
-                const m = new EventMiddleware('test', function (next) {
-                    next();
-                });
-                m.post(function() {
+            it('should catch error from post function', function() {
+                const m1 = new EventMiddleware('test', next => next());
+                m1.post(function() {
                     throw new Error('error');
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
-            });
-            it('should catch error when post function return error by next callback', function() {
-                const m = new EventMiddleware('test', next => next());
-                m.post(function(next) {
+
+                const m2 = new EventMiddleware('test', next => next());
+                m2.post(function(next) {
                     next(new Error('error'));
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
-            });
 
-            it('should catch error when post function return error by promise', function() {
-                const m = new EventMiddleware('test', next => next());
-                m.post(function() {
+                const m3 = new EventMiddleware('test', next => next());
+                m3.post(function() {
                     return Promise.reject(new Error('error'));
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
+
+                return Promise.all([
+                    m1.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    }),
+                    m2.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    }),
+                    m3.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    })
+                ]);
             });
 
-            it('should catch error when main function throw error', function() {
-                const m = new EventMiddleware('test', next => {
+            it('should catch error from main function', function() {
+                const m1 = new EventMiddleware('test', next => {
                     throw new Error('error');
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
-            });
 
-            it('should catch error when main function return error by next callback', function() {
-                const m = new EventMiddleware('test', next => {
+                const m2 = new EventMiddleware('test', next => {
                     next(new Error('error'));
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
-            });
 
-            it('should catch error when main function return error by promise', function() {
-                const m = new EventMiddleware('test', next => {
+                const m3 = new EventMiddleware('test', next => {
                     return Promise.reject(new Error('error'));
                 });
-                return m.call().catch(err => {
-                    err.should.be.Error();
-                    err.message.should.be.eql('error');
-                });
+
+                return Promise.all([
+                    m1.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    }),
+                    m2.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    }),
+                    m3.call().catch(err => {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                    })
+                ]);
             });
         });
 
         describe('catch', function() {
-            it('should catch main function error', function() {
+            it('should catch main function error', function(done) {
+                const m1 = new EventMiddleware('test', next => {
+                    throw new Error('error');
+                });
+
+                const m2 = new EventMiddleware('test', next => {
+                    next(new Error('error'));
+                });
+
+                const m3 = new EventMiddleware('test', next => {
+                    return Promise.reject(new Error('error'));
+                });
+
+                m3.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m2.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        m3.call();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m1.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        m2.call();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m1.call();
             });
 
-            it('should catch pre function error', function() {
+            it('should catch pre function error', function(done) {
+                const m1 = new EventMiddleware('test', noop);
+                m1.pre(() => {
+                    throw new Error('error');
+                });
 
+                const m2 = new EventMiddleware('test', noop);
+                m2.pre(next => {
+                    next(new Error('error'));
+                });
+
+                const m3 = new EventMiddleware('test', noop);
+                m3.pre(() => {
+                    return Promise.reject(new Error('error'));
+                });
+
+                m3.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m2.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        m3.call();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m1.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        m2.call();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m1.call();
             });
 
-            it('should catch post function error', function() {
+            it('should catch post function error', function(done) {
+                const m1 = new EventMiddleware('test', next => next());
+                m1.post(() => {
+                    throw new Error('error');
+                });
 
+                const m2 = new EventMiddleware('test', next => next());
+                m2.post(next => {
+                    next(new Error('error'));
+                });
+
+                const m3 = new EventMiddleware('test', next => next());
+                m3.post(() => {
+                    return Promise.reject(new Error('error'));
+                });
+
+                m3.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        done();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m2.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        m3.call();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m1.catch(err => {
+                    try {
+                        err.should.be.Error();
+                        err.message.should.be.eql('error');
+                        m2.call();
+                    } catch (e) {
+                        done(e);
+                    }
+                });
+                m1.call();
+            });
+        });
+
+        describe('setOptions', function() {
+            it('should work', function() {
+                const m = new EventMiddleware('test', noop);
+                const options = {
+                    multiArgs: false,
+                    globalArgs: true
+                };
+                m._options.multiArgs.should.not.be.eql(options.multiArgs);
+                m._options.globalArgs.should.not.be.eql(options.globalArgs);
+                m.setOptions(options);
+                m._options.multiArgs.should.be.eql(options.multiArgs);
+                m._options.globalArgs.should.be.eql(options.globalArgs);
             });
         });
 
